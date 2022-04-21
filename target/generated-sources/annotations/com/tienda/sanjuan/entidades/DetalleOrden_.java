0b78b418056734843f1c6820a1d5fe57110589b2
Caroep <caritop1988@gmail.com>
2022-04-21 19:11:00 -0300

    backend caro

--- target/generated-sources/annotations/com/tienda/sanjuan/entidades/DetalleOrden_.java
+++ target/generated-sources/annotations/com/tienda/sanjuan/entidades/DetalleOrden_.java
@@ -1,18 +1,17 @@
 package com.tienda.sanjuan.entidades;
 
-import com.tienda.sanjuan.entidades.Usuario;
+import com.tienda.sanjuan.entidades.Orden;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-04-15T15:40:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-04-21T19:03:44")
 @StaticMetamodel(DetalleOrden.class)
 public class DetalleOrden_ { 
 
     public static volatile SingularAttribute<DetalleOrden, Double> total;
     public static volatile SingularAttribute<DetalleOrden, Integer> quantity;
     public static volatile SingularAttribute<DetalleOrden, String> id;
-    public static volatile SingularAttribute<DetalleOrden, String> orden;
-    public static volatile SingularAttribute<DetalleOrden, Usuario> user;
+    public static volatile SingularAttribute<DetalleOrden, Orden> orden;
 
 }
--- target/generated-sources/annotations/com/tienda/sanjuan/entidades/Orden_.java
+++ target/generated-sources/annotations/com/tienda/sanjuan/entidades/Orden_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-04-15T15:40:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-04-21T19:03:44")
 @StaticMetamodel(Orden.class)
 public class Orden_ { 
 
