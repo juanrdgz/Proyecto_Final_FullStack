ec66de0d66847239007e3aa2735c850c7dd43a74
Caroep <caritop1988@gmail.com>
2022-05-02 21:56:49 -0300

    problema repositorio material

--- src/main/java/com/tienda/sanjuan/repositorios/ArticuloRepositorio.java
+++ src/main/java/com/tienda/sanjuan/repositorios/ArticuloRepositorio.java
@@ -10,12 +10,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Equipo6
  */
-
+@Repository
 public interface ArticuloRepositorio extends JpaRepository<Articulo, String>{
     
      @Query("select a from Articulo a where a.title= :title")
